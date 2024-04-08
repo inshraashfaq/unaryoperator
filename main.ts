57b61bdf9f7ff47681a5f68c44ac195749190729
inshraashfaq <166373896+inshraashfaq@users.noreply.github.com>
2024-04-08 13:44:36 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,43 @@
+// unary operator:
+// pre Increment (++)
+
+// let a = 10
+// let b = 6
+// ++a //  11
+// console.log(a) //11
+// console.log(a) //11
+// console.log(++a) //12
+// console.log(a)   //12
+
+// post Increment (++)
+// let a = 10
+// let b = 6
+// a++ //  11
+// console.log(a) //11
+// console.log(a) //11
+// console.log(a++) //11
+// console.log(a)   //12
+
+// pre Decrement (--)
+// let a = 10
+// let b = 6
+// --a //  9
+// console.log(a) //9
+// console.log(a) //9
+// console.log(--a) //9 - 1 = 8
+// console.log(a) // 8
+
+// post Decrement (--)
+// let a = 10 
+// let b = 6
+// a-- // 9
+// console.log(a) // 9
+// console.log(a) // 9
+// console.log(a--) // 9
+// console.log(a) // 8
+
+// let a = 10
+// let b = 6
+// let c = a++ + ++a + ++b + b++ + b + a-- + b--
+//         //10 + 12 + 7 + 7 + 8 + 12 + 8
+// console.log(c)
